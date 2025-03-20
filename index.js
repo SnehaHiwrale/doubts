fd87b96483a9c889d3108b07e0181f5a00e84c2b
Sneha <snehahiwrale358@gmail.com>
2025-03-20 16:31:53 +0530

    forntend added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const { protectedRoute } = require("./middleware/auth.middleware")
 require("dotenv").config()
+const path = require("path")
 
 const app = express()
 
@@ -14,7 +15,8 @@ app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/blog", protectedRoute, require("./routes/blog.routes"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "resource not found" })
 })
 
 mongoose.connect(process.env.MONGO_URL)
