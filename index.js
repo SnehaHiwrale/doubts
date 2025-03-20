1c40b3db0f19dc69bae8fd08c982ada3cde42731
Sneha <snehahiwrale358@gmail.com>
2025-03-20 16:40:00 +0530

    update forntend

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const path = require("path")
 
 const app = express()
 
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: true, credentials: true }))
